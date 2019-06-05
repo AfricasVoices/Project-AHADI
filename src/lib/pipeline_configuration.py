8a622e159f88d388783f4d5baa2b5a5115d39f01
Alexander Simpson <as2388@cam.ac.uk>
2019-06-05 15:52:40 +0100

    Output to Coda/production

--- src/lib/pipeline_configuration.py
+++ src/lib/pipeline_configuration.py
@@ -1,7 +1,7 @@
 import json
 from urllib.parse import urlparse
 
-from core_data_modules.cleaners import somali, Codes
+from core_data_modules.cleaners import Codes
 from core_data_modules.data_models import Scheme, validators
 from dateutil.parser import isoparse
 
@@ -13,7 +13,15 @@ def _open_scheme(filename):
 
 
 class CodeSchemes(object):
-    pass
+    S01E01_REASONS = _open_scheme("s01e01_reasons.json")
+    S01E02_REASONS = _open_scheme("s01e02_reasons.json")
+    S01E03_REASONS = _open_scheme("s01e03_reasons.json")
+    S01E04_REASONS = _open_scheme("s01e04_reasons.json")
+
+    GENDER = _open_scheme("gender.json")
+    AGE = _open_scheme("age.json")
+
+    # WS_CORRECT_DATASET = _open_scheme("ws_correct_dataset.json")
 
 
 class CodingPlan(object):
@@ -39,26 +47,63 @@ class CodingPlan(object):
 
 
 class PipelineConfiguration(object):
-    RQA_CODING_PLANS = []
+    RQA_CODING_PLANS = [
+        CodingPlan(raw_field="rqa_s01e01_raw",
+                   coded_field="rqa_s01e01_coded",
+                   time_field="sent_on",
+                   coda_filename="s01e01.json",
+                   icr_filename="s01e01.csv",
+                   run_id_field="rqa_s01e01_run_id",
+                   analysis_file_key="rqa_s01e01_",
+                   code_scheme=CodeSchemes.S01E01_REASONS),
 
-    @staticmethod
-    def clean_age_with_range_filter(text):
-        """
-        Cleans age from the given `text`, setting to NC if the cleaned age is not in the range 10 <= age < 100.
-        """
-        age = somali.DemographicCleaner.clean_age(text)
-        if type(age) == int and 10 <= age < 100:
-            return str(age)
-            # TODO: Once the cleaners are updated to not return Codes.NOT_CODED, this should be updated to still return
-            #       NC in the case where age is an int but is out of range
-        else:
-            return Codes.NOT_CODED
+        CodingPlan(raw_field="rqa_s01e02_raw",
+                   coded_field="rqa_s01e02_coded",
+                   time_field="sent_on",
+                   coda_filename="s01e02.json",
+                   icr_filename="s01e02.csv",
+                   run_id_field="rqa_s01e02_run_id",
+                   analysis_file_key="rqa_s01e02_",
+                   code_scheme=CodeSchemes.S01E02_REASONS),
+
+        CodingPlan(raw_field="rqa_s01e03_raw",
+                   coded_field="rqa_s01e03_coded",
+                   time_field="sent_on",
+                   coda_filename="s01e03.json",
+                   icr_filename="s01e03.csv",
+                   run_id_field="rqa_s01e03_run_id",
+                   analysis_file_key="rqa_s01e03_",
+                   code_scheme=CodeSchemes.S01E03_REASONS),
+
+        CodingPlan(raw_field="rqa_s01e04_raw",
+                   coded_field="rqa_s01e04_coded",
+                   time_field="sent_on",
+                   coda_filename="s01e04.json",
+                   icr_filename="s01e04.csv",
+                   run_id_field="rqa_s01e04_run_id",
+                   analysis_file_key="rqa_s01e04_",
+                   code_scheme=CodeSchemes.S01E04_REASONS),
+    ]
 
     LOCATION_CODING_PLANS = []
 
     SURVEY_CODING_PLANS = []
     SURVEY_CODING_PLANS.extend(LOCATION_CODING_PLANS)
-    SURVEY_CODING_PLANS.extend([])
+    SURVEY_CODING_PLANS.extend([
+        CodingPlan(raw_field="gender_raw",
+                   coded_field="gender_coded",
+                   time_field="gender_time",
+                   coda_filename="gender.json",
+                   analysis_file_key="gender",
+                   code_scheme=CodeSchemes.GENDER),
+
+        CodingPlan(raw_field="age_raw",
+                   coded_field="age_coded",
+                   time_field="age_time",
+                   coda_filename="age.json",
+                   analysis_file_key="age",
+                   code_scheme=CodeSchemes.AGE)
+    ])
 
     def __init__(self, rapid_pro_domain, rapid_pro_token_file_url, activation_flow_names, survey_flow_names,
                  rapid_pro_test_contact_uuids, phone_number_uuid_table, recovery_csv_urls, rapid_pro_key_remappings,
